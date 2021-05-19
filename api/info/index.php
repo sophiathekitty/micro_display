baf302894bd007b433ae46c017a30760ea49c226
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-05-19 00:48:55 -0600

    fixing the info api

--- api/info/index.php
+++ api/info/index.php
@@ -3,11 +3,14 @@ require_once("../../includes/main.php");
 $data = [
 	'info' => [
 		'url' => $_SERVER['HTTP_HOST'],
-		'path' => "/extensions"."/".LoadSettingVar('extension_path'),
-		'type' => LoadSettingVar('extension_type'),
-		'enabled' => LoadSettingVar('extension_enabled'),
+		'room' => LoadSettingVar('room_id'),
+		'type' => LoadSettingVar('type'),
+		'enabled' => LoadSettingVar('enabled'),
+		'main' => LoadSettingVar('main'),
+		'path' => LoadSettingVar('path'),
+		'server' => LoadSettingVar('server'),
 		'mac_address' => LocalMacAddress(),
-		'name' => LoadSettingVar('extension_name')
+		'name' => LoadSettingVar('name')
 		]
 	];
 OutputJson($data);
