ff609ede7a357bae4f53f5395f7e8abb6044af05
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-05-19 01:22:27 -0600

    adding echos so i can debug

--- modules/servers/power.php
+++ modules/servers/power.php
@@ -1,14 +1,15 @@
 <?php
 function ShutdownDevice(){
     SaveSettingVar('shutdown_requested',0);
-    shell_exec("sudo shutdown now");
+    shell_exec("sudo shutdown +1");
 }
 function RebootDevice(){
     SaveSettingVar('reboot_requested',0);
-    shell_exec("sudo shutdown -r now");
+    shell_exec("sudo shutdown -r +1");
 }
 function UpdateFromGit(){
     SaveSettingVar("check_for_update",0);
+    echo "we're doing the shell script maybe?\n";
     echo shell_exec("sh /var/www/html/gitpull.sh");
 }
 ?>
