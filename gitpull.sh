93f0ccc9c234ba1eb4fa40f47813fc0c933ab640
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-12-17 00:45:16 -0700

    update fonts

--- gitpull.sh
+++ gitpull.sh
@@ -1,3 +1,4 @@
 cd /var/www/html
+sudo chmod 777 -R .git/
 git reset --hard
 git pull
--- python/pitft/screen.sh
+++ python/pitft/screen.sh
@@ -3,18 +3,12 @@ echo "-----------------------------------"
 echo "waiting to start (2 minutes)"
 echo "-----------------------------------"
 sleep 2m
+echo "-----------------------------------"
+echo "starting python"
+echo "-----------------------------------"
 while true; do
-    echo "-----------------------------------"
-    echo "starting python script"
-    echo "-----------------------------------"
     sudo python3 /var/www/html/python/pitft/screen.py &
     wait $!
-    echo "-----------------------------------"
-    echo "python script crashed (waiting)"
-    echo "-----------------------------------"
-    sleep 5m
-    echo "-----------------------------------"
-    echo "ready to restart"
-    echo "-----------------------------------"
+    sleep 10
 done
 exit
